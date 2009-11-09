7fdc0dc06b6992f75d59bdb49c145e8a223f931c
Chris Williams <chris@iterativedesigns.com>
2009-11-08 22:38:16 -0500

    Better structure.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,3 +10,10 @@ role :web, "jsconf.us"
 role :app, "jsconf.us"
 role :db,  "jsconf.us"
 
+namespace :deploy do
+  task :start {}
+  task :stop {}
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    
+  end
+end
