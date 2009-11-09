5f51d03ee742ddff769f636b485fd0c0f6acf6e8
Chris Williams <chris@iterativedesigns.com>
2009-11-08 22:33:48 -0500

    Deployment help.

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+set :application, "JSConf FTW"
+set :repository,  "git://github.com/voodootikigod/jsconfus.git"
+
+set :scm, :git
+
+
+role :web, "jsconf.us"
+role :app, "jsconf.us"
+role :db,  "jsconf.us"
+
