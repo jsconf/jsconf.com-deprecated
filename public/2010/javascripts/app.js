6c5748199e4c3118f35d243c99f9606eb1d8246a
Chris Williams <chris@iterativedesigns.com>
2010-01-08 17:17:47 -0500

    2010

--- public/2010/javascripts/app.js
+++ public/2010/javascripts/app.js
@@ -1,93 +1,89 @@
-dojo.require("dojo.dnd.Mover");
-dojo.require("dojo.dnd.Moveable");
-dojo.require("dojo.dnd.move");
-dojo.require("dojox.fx");
-dojo.require("dojox.fx.easing");
-dojo.require("dijit._base.place");
-
 ;var jsconf = (function () {
     var targeted_id = 'home';
-    var header_height = 40;
+    var template = "<div class='article'><h3><a href='{{link}}'>{{title}}</a></h3><div class='dateline'>{{date}}</div><div class='body'>{{{body}}}</div>";
     
+    var panel_width = 870;
+    var left_edge = function() {
+        return jQuery("#navigation").offset().left;
+    };
     var coords_for = function (id) {
-	var x = 0;
-	var y = 0;
-	if (id == "articles" || id == "schedule" || id== "videos") {
-	    y = header_height;
-	} else if (id == "register" || id == "home" || id== "sponsors") {
-	    y = -(549 - header_height);
-	} else if (id == "about" || id == "speakers" || id== "venue") {
-	    y = -(1109 - header_height);
-        }
-	var navleft = dojo.query("#navigation").coords()[0].x;
-	if (id == "articles" || id=="register" || id == "about")	{
-	    x = navleft - 169;
-	} else if (id == "schedule" || id=="home" || id == "speakers")	{
-	    x = navleft - 1069;
-	} else if (id == "videos" || id=="sponsors" || id == "venue")	{
-	    x = navleft - 1969;
-	}
-        return {"top": y, "left": x};
-    }
+      var x = 0;
+      var y = 0;
+      if (id == "articles" || id == "schedule" || id== "videos") {
+          y = 110;
+      } else if (id == "register" || id == "home" || id== "sponsors") {
+          y = -439;
+      } else if (id == "about" || id == "speakers" || id== "venue") {
+          y = -990;
+      }
+      var navleft = left_edge();
+      if (id == "articles" || id=="register" || id == "about")	{
+          x = navleft + 10;
+      } else if (id == "schedule" || id=="home" || id == "speakers")	{
+          x = navleft - (panel_width);
+      } else if (id == "videos" || id=="sponsors" || id == "venue")	{
+          x = navleft - ((panel_width * 2) +13);
+      }
+      var obj = {"top": y, "left": x};
+      return obj;
+    };
     
     var refocus = function(easing) {
-	var new_coords = coords_for(targeted_id);
-	if (easing) {
-	    dojo.fx.slideTo({
-		node: "viewpane", // drag node
-		top: new_coords.top, // tmp top
-		left: new_coords.left, // tmp left
-		easing: dojox.fx.easing.cubicOut,
-		duration:950 // ms
-	    }).play(5);
-	} else {
-	    dojo.style("viewpane", new_coords);
-	}
-	
-
+      var new_coords = coords_for(targeted_id);
+      if (easing) {
+         jQuery("#viewpane").animate(new_coords);
+      } else {
+        jQuery("#viewpane").css(new_coords);
+      }
     };
 
     return {
-	resizeContainer: function() {
-	    header_height = dojo.query("#navigation").coords()[0].h;
-	    var vp = dijit.getViewport();
-	    dojo.style("container", {
-		"height": vp.h,
-		"width": vp.w
-	    });
-	    refocus();
-	},
-	focus_on: function(id) {
-	    targeted_id = id;
-	    refocus(true);
-	},
-        default_focus: function () {
-            refocus(true);
-        }
-	
+      resizeContainer: function() {
+        refocus();
+      },
+      focus_on: function(id) {
+        targeted_id = id;
+        refocus(true);
+      },
+      default_focus: function () {
+        refocus(true);
+      },
+      load_blog: function() {
+        var blog_feed = new google.feeds.Feed("http://jsconf.posterous.com/rss.xml");
+        blog_feed.setNumEntries(10); //Google Feed API method
+        blog_feed.load(function(result) {
+          console.log(result);
+          if (!result.error) {
+            result.feed.entries.forEach(function(e,i){
+              jQuery("#article_listing").append(Mustache.to_html(template, {title: e.title, body: e.content, date: Date.parse(e.publishedDate).toString('dddd, MMMM d, yyyy'), link: e.link }));
+            });
+            $("#article_listing .article p:last").remove();
+          }
+        });
+      }
     }
 })();
 
-var t = null;
 
-dojo.addOnLoad(function() {
-    jsconf.resizeContainer();
-    dojo.connect(window, "onresize", jsconf.resizeContainer);
-    // dojo.connect(, "onresize", jsconf.resizeContainer);
-    dojo.query("div.block").onclick(function(e) {
-        var curr_node = e.target;
-        t = curr_node;
-        while( curr_node.className != "block") {
-            curr_node = curr_node.parentNode;
-        }
-	jsconf.focus_on(curr_node.id);
-    });
-    dojo.query("textarea").onclick(function(e) {
-        e.target.focus();
-    });
-    var viewpane = new dojo.dnd.Moveable("viewpane");
-    dojo.subscribe("/dnd/move/stop", function(mover){ 
-        jsconf.default_focus();;
-    });
 
-});
+google.load("feeds", "1");
+google.load("jquery", "1");
+
+
+var initialize = function() {
+  jsconf.resizeContainer();
+  jQuery(window).resize(jsconf.resizeContainer);
+  jQuery("div.block").click(function(e) {
+    jsconf.focus_on(jQuery(this).attr("id"));
+  });
+  jQuery("#navigation a, a.focus").click(function(e) {
+    e.preventDefault(); e.stopPropagation();
+    jsconf.focus_on(jQuery(this).attr("href").replace("#",""));
+  });
+  jsconf.load_blog();
+};
+
+
+// jQuery(document).ready(initialize);
+
+google.setOnLoadCallback(initialize);
