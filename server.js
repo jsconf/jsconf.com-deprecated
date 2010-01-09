273c9a4909936ef756e3edf7bc4e37e54bb04250
Chris Williams <chris@iterativedesigns.com>
2010-01-09 01:29:56 -0500

    Server update.

--- server.js
+++ server.js
@@ -28,24 +28,24 @@ fu.post("/app/register_speaker", function (req, res) {
     req.addListener("complete", function() {
         try {
 						data = data.split("=")[1];
-            var clean_data = decodeURIComponent(data); //.replace(/\%40/g, "@").replace(/\%3A/g, ":").replace(/\%2C/g, ",").replace("submission=", "").replace(/\)$/, "");
+            var clean_data = decodeURIComponent(data.replace(/\+/g," ")); //.replace(/\%40/g, "@").replace(/\%3A/g, ":").replace(/\%2C/g, ",").replace("submission=", "").replace(/\)$/, "");
             var reg_data = JSON.parse(clean_data);
             reg_data.category="speaker";
 						reg_data.created_at = (new Date());
             if (isblank(reg_data.name) || isblank(reg_data.twitter) || isblank(reg_data.email) || isblank(reg_data.location) || isblank(reg_data.topic_title) || isblank(reg_data.topic_description) || isblank(reg_data.claim_to_fame)) {
   							save_failed(data);
-                res.simpleJSON(400, { message: "If you follow the directions, there will be cake." });
+                res.simpleJSON(200, { message: "If you follow the directions, there will be cake." });
             } else {
                 couchdb.saveDoc(reg_data, {success: function(doc) {
-                    res.simpleJSON(201, { message: "We got yer submission. ARR, we be gettin' back to you soon." });
+                    res.simpleJSON(200, { message: "We got yer submission. ARR, we be gettin' back to you soon." });
                 }, error: function(result) {
 										save_failed(data);
-                    res.simpleJSON(400, { message: "MUTINY WILL NOT BE TOLERATED!!!" });  
+                    res.simpleJSON(200, { message: "MUTINY WILL NOT BE TOLERATED!!!" });  
                 }});
             }
         } catch (e) {
 						save_failed(data);
-            res.simpleJSON(400, { message: "MUTINY WILL NOT BE TOLERATED!!!" });      
+            res.simpleJSON(200, { message: "MUTINY WILL NOT BE TOLERATED!!!" });      
         }
     });
 });
