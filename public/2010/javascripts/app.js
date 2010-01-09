a974b242c41357a7ed279d81da3889153645164d
Chris Williams <chris@iterativedesigns.com>
2010-01-09 00:11:20 -0500

    Updating feedburner

--- public/2010/javascripts/app.js
+++ public/2010/javascripts/app.js
@@ -49,7 +49,7 @@
         refocus(true);
       },
       load_blog: function() {
-        var blog_feed = new google.feeds.Feed("http://feeds.feedburner.com/posterous/mkEj");
+        var blog_feed = new google.feeds.Feed("http://feeds.feedburner.com/posterous/tzwp");
         blog_feed.setNumEntries(10); //Google Feed API method
         blog_feed.load(function(result) {
           if (!result.error) {
