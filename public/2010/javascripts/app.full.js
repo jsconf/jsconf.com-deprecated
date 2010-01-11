1398e10bf6f5862fdbcbd47840558d09bfce37fe
Chris Williams <chris@iterativedesigns.com>
2010-01-11 09:34:42 -0500

    Update

--- public/2010/javascripts/app.full.js
+++ public/2010/javascripts/app.full.js
@@ -1228,8 +1228,8 @@ return((r[1].length===0)?r[0]:null);};};Date.parseExact=function(s,fx){return Da
         jQuery("#home").each(function() {
 
           jsconf.load_blog(1, function(result) {
-            jQuery.each(result.feed.entries, function(e,i){
-              jQuery("#top_article").append(Mustache.to_html(article_template, {title: e.title, body: e.contentSnippet.replace("Permalink", ""), date: Date.parse(e.publishedDate).toString('dddd, MMMM d, yyyy'), link: e.link }));
+            jQuery.each(result.feed.entries, function(){
+              jQuery("#top_article").append(Mustache.to_html(article_template, {title: this.title, body: this.contentSnippet.replace("Permalink", ""), date: Date.parse(this.publishedDate).toString('dddd, MMMM d, yyyy'), link: this.link }));
             });
           });
         });
@@ -1237,7 +1237,7 @@ return((r[1].length===0)?r[0]:null);};};Date.parseExact=function(s,fx){return Da
         jQuery("#article_listing").each(function() {
           jsconf.load_blog(10, function(result) {
             jQuery.each(result.feed.entries, function(e,i){
-              jQuery("#article_listing").append(Mustache.to_html(article_template, {title: e.title, body: e.content, date: Date.parse(e.publishedDate).toString('dddd, MMMM d, yyyy'), link: e.link }));
+              jQuery("#article_listing").append(Mustache.to_html(article_template, {title: this.title, body: this.content, date: Date.parse(this.publishedDate).toString('dddd, MMMM d, yyyy'), link: this.link }));
             });
             $("#article_listing .article p:last").remove();
           });
